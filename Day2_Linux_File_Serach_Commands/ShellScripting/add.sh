fbe0f4b3f72ca4390c2a7026c97484d57790b206
javeed512 <javeedmohammedjaveed@gmail.com>
2025-04-25 13:33:24 +0530

    awk commands , shellscript PDFs,demos

---
+++ Day2_Linux_File_Serach_Commands/ShellScripting/add.sh
@@ -0,0 +1,16 @@
+
+
+
+echo "Enter n1 value"
+read n1
+
+echo "Enter n2 value"
+read n2
+
+#let result=$n1+$n2 
+
+result=$((n1+n2))
+
+echo  "Result is:  $result"
+
+
---
+++ Day2_Linux_File_Serach_Commands/ShellScripting/hello.sh
@@ -0,0 +1,16 @@
+
+
+
+echo  "Enter your name"
+read name
+
+echo "Enter your qualification"
+read  q1
+
+echo  "Name : $name     Qualification : $q1"
+
+
+
+
+
+
